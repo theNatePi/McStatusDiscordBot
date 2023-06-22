85f92d6f8a75a9c861e83910106725d570ea4733
Nathan P <natepie04@gmail.com>
2023-06-22 14:03:52 -0700

    Seperated starting any looped tasks

--- run.py
+++ run.py
@@ -4,11 +4,17 @@ import cogs
 from discord.ext import commands
 
 
-COGS = [cogs.Standard, cogs.McStatus]
+COGS = [cogs.Standard, cogs.McStatus, cogs.LoopedTasks]
 """All cogs to add to the bot"""
 
 
-def _define_on_ready(bot):
+def _start_background_tasks(cogs_to_start):
+    for cog in cogs_to_start:
+        if hasattr(cog, 'start_tasks'):
+            cog.start_tasks()
+
+
+def _define_on_ready(bot, added_cogs):
     """
     Defines the "on_ready" event for the bot
     :param bot: commands.Bot object
@@ -16,6 +22,7 @@ def _define_on_ready(bot):
     @bot.event
     async def on_ready():
         print('Ready!')
+        _start_background_tasks(added_cogs)
 
 
 async def _add_cogs(bot):
@@ -23,8 +30,12 @@ async def _add_cogs(bot):
     Adds all cogs stored in COGS
     :param bot: commands.Bot object
     """
+    initialized_cogs = []
     for cog in COGS:
-        await bot.add_cog(cog(bot))
+        initialized_cog = cog(bot)
+        initialized_cogs.append(initialized_cog)
+        await bot.add_cog(initialized_cog)
+    return initialized_cogs
 
 
 if __name__ == "__main__":
@@ -32,6 +43,6 @@ if __name__ == "__main__":
                                 description = config.BOT_DESCRIPTION,
                                 intents = config.INTENTS)
 
-    _define_on_ready(mcstatus_bot)
-    asyncio.run(_add_cogs(mcstatus_bot))
+    cogs = asyncio.run(_add_cogs(mcstatus_bot))
+    _define_on_ready(mcstatus_bot, cogs)
     mcstatus_bot.run(config.TOKEN)
