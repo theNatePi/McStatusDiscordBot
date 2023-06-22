024334994d51900f0f733e1cf0decea4dc4a9a82
Nathan P <natepie04@gmail.com>
2023-06-22 00:32:48 -0700

    Added docstrings

--- config/bot_config.py
+++ config/bot_config.py
@@ -1,8 +1,12 @@
+"""
+All config variables for the bot as a whole
+"""
 import discord
 
 PREFIX = '!'
 
 INTENTS = discord.Intents.default()
+"""Intents used by the bot"""
 INTENTS.messages = True
 
 BOT_DESCRIPTION = 'A bot which can be configured to display information ' \
