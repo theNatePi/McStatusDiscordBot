130172d1d9945a1ebc079c229bd8113a977ec5b2
Nathan P <natepie04@gmail.com>
2023-06-22 13:23:19 -0700

    Added !online command
    Added command to McStatus cog in mcstatus_commands.py.
    
    Added description and error message to bot_config.py.

---
+++ cogs/mcstatus_commands.py
@@ -0,0 +1,24 @@
+import config
+from discord.ext import commands
+from mcstatus import JavaServer
+
+
+class McStatus(commands.Cog, name = config.MCSTATUS_COG_NAME):
+    """Standard commands which can be used to interact with the bot"""
+
+    def __init__(self, bot):
+        self._bot = bot
+
+
+    @commands.command(name = 'online', description = config.ONLINE_DESCRIPTION, help = config.ONLINE_HELP)
+    async def _online(self, ctx):
+        original_message = await ctx.send('Loading...')
+        try:
+            server = JavaServer('24.199.71.94', 25565)
+            status = server.status()
+            users_connected = ', '.join([user['name'] for user in status.raw['players']['sample']])
+            final_message = f"The following players are online!\n{users_connected}"
+            await original_message.edit(content = final_message)
+        except Exception as exc:
+            print(f"ERROR during !online: {exc}")
+            await original_message.edit(content = config.ONLINE_ERROR)
--- config/bot_config.py
+++ config/bot_config.py
@@ -15,7 +15,12 @@ BOT_DESCRIPTION = 'A bot which can be configured to display information ' \
 
 # Names for cogs
 STANDARD_COG_NAME = 'Standard Commands'
+MCSTATUS_COG_NAME = 'Server Status Commands'
 
 # Help descriptions for commands
 PING_DESCRIPTION = 'Sends a ping to the bot to ensure it is working'
 PING_HELP = 'Should respond with "pong"'
+
+ONLINE_DESCRIPTION = 'Retrieves all currently online players'
+ONLINE_HELP = 'Responds with a list of players'
+ONLINE_ERROR = "Sorry, I couldn't get the online players :("
