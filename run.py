fdfdeddde1f00c5c8241e387e4c1706fd968a25b
Nathan P <natepie04@gmail.com>
2023-06-22 01:06:52 -0700

    _add_cogs now uses async

--- run.py
+++ run.py
@@ -1,8 +1,10 @@
+import asyncio
 import config
+import cogs
 from discord.ext import commands
 
 
-COGS = []
+COGS = [cogs.Standard]
 """All cogs to add to the bot"""
 
 
@@ -16,13 +18,13 @@ def _define_on_ready(bot):
         print('Ready!')
 
 
-def _add_cogs(bot):
+async def _add_cogs(bot):
     """
     Adds all cogs stored in COGS
     :param bot: commands.Bot object
     """
     for cog in COGS:
-        bot.add_cog(cog(mcstatus_bot))
+        await bot.add_cog(cog(bot))
 
 
 if __name__ == "__main__":
@@ -31,5 +33,5 @@ if __name__ == "__main__":
                                 intents = config.INTENTS)
 
     _define_on_ready(mcstatus_bot)
-    _add_cogs(mcstatus_bot)
+    asyncio.run(_add_cogs(mcstatus_bot))
     mcstatus_bot.run(config.TOKEN)
