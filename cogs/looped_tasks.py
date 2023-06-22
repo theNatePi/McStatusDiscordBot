70f170ff4639cc59bb52dae38bd24974b4dfd46a
Nathan P <natepie04@gmail.com>
2023-06-22 14:25:24 -0700

    Added error handling in update_activity

--- cogs/looped_tasks.py
+++ cogs/looped_tasks.py
@@ -16,10 +16,16 @@ class LoopedTasks(commands.Cog):
                                         name = "!online / !help")
             self._current_activity = 1
         elif self._current_activity == 1:
-            server = connect_to_server()
-            num_players = num_players_online(server)
-            activity = discord.Activity(type = discord.ActivityType.watching,
-                                        name = f"{num_players} people online!")
+            try:
+                server = connect_to_server()
+                num_players = num_players_online(server)
+                activity = discord.Activity(type = discord.ActivityType.watching,
+                                            name = f"{num_players} people online!")
+            except Exception as exc:
+                activity = discord.Activity(type = discord.ActivityType.watching,
+                                            name = 'ERROR')
+                print(f'ERROR in activity update, {exc}')
+
             self._current_activity = 0
 
         await self._bot.change_presence(status=discord.Status.online, activity=activity)
