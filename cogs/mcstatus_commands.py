713479c47ccb301f2ed46e7185f8e4cb67d41181
Nathan P <natepie04@gmail.com>
2023-06-22 14:22:52 -0700

    !online now sends embed
    Added _create_online_embed in mcstatus_commands.py to create the embed.

--- cogs/mcstatus_commands.py
+++ cogs/mcstatus_commands.py
@@ -1,3 +1,4 @@
+import discord
 import config
 from discord.ext import commands
 from mcstatus import JavaServer
@@ -28,15 +29,31 @@ class McStatus(commands.Cog, name = config.MCSTATUS_COG_NAME):
         self._bot = bot
 
 
+    @staticmethod
+    def _create_online_embed(players):
+        if players:
+            players_formatted = '\n'.join(players)
+            players_formatted = f'```\n{players_formatted}\n```'
+
+            embed = discord.Embed(title = 'The following players are online!',
+                                  color = discord.Color.green(),
+                                  description = players_formatted)
+        else:
+            embed = discord.Embed(title = 'No one is online!',
+                                  color = discord.Color.red())
+
+        return embed
+
+
     @commands.command(name = 'online', description = config.ONLINE_DESCRIPTION, help = config.ONLINE_HELP)
     async def _online(self, ctx):
         original_message = await ctx.send('Loading...')
         try:
             server = connect_to_server()
             players = players_list(server)
-            users_connected = ', '.join(players)
-            final_message = f"The following players are online!\n{users_connected}"
-            await original_message.edit(content = final_message)
+
+            embed = self._create_online_embed(players)
+            await original_message.edit(content = None, embed = embed)
         except Exception as exc:
             print(f"ERROR during !online: {exc}")
             await original_message.edit(content = config.ONLINE_ERROR)
