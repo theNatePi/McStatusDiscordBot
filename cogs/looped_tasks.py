d9e9297e2f91f8398ce903889aeab970cafa107b
Nathan P <natepie04@gmail.com>
2023-06-22 14:05:37 -0700

    Added activity updating
    Loops every 10 seconds to update the activity.
    
    Loops between:
    !online command
    How many players are online

---
+++ cogs/looped_tasks.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands, tasks
+from .mcstatus_commands import connect_to_server, num_players_online
+
+class LoopedTasks(commands.Cog):
+    def __init__(self, bot):
+        self._bot = bot
+        self._current_activity = 0
+
+
+    @tasks.loop(seconds = 10)
+    async def update_activity(self):
+        activity = None
+        if self._current_activity == 0:
+            activity = discord.Activity(type = discord.ActivityType.listening, name = "!online")
+            self._current_activity = 1
+        elif self._current_activity == 1:
+            server = connect_to_server()
+            num_players = num_players_online(server)
+            activity = discord.Activity(type = discord.ActivityType.watching,
+                                        name = f"{num_players} people online!")
+            self._current_activity = 0
+
+        await self._bot.change_presence(status=discord.Status.online, activity=activity)
+
+
+    def start_tasks(self):
+        self.update_activity.start()
