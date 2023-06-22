98c64be877b296238d6a5d30595ef44527f9db1f
Nathan P <natepie04@gmail.com>
2023-06-22 12:20:19 -0700

    Updated command and cog descriptions

--- cogs/standard_commands.py
+++ cogs/standard_commands.py
@@ -1,13 +1,14 @@
+import config
 from discord.ext import commands
 
 
-class Standard(commands.Cog):
+class Standard(commands.Cog, name = config.STANDARD_COG_NAME):
     """Standard commands which can be used to interact with the bot"""
 
     def __init__(self, bot):
         self._bot = bot
 
-    # description=config.HELP_CONNECT_LONG, help=config.HELP_CONNECT_SHORT
-    @commands.command(name='ping')
+
+    @commands.command(name = 'ping', description = config.PING_DESCRIPTION, help = config.PING_HELP)
     async def _ping(self, ctx):
         await ctx.send('pong')
--- config/bot_config.py
+++ config/bot_config.py
@@ -12,3 +12,10 @@ INTENTS = discord.Intents.all()
 
 BOT_DESCRIPTION = 'A bot which can be configured to display information ' \
                   'about users on a specific Minecraft Server'
+
+# Names for cogs
+STANDARD_COG_NAME = 'Standard Commands'
+
+# Help descriptions for commands
+PING_DESCRIPTION = 'Sends a ping to the bot to ensure it is working'
+PING_HELP = 'Should respond with "pong"'
