79fb5a20ff75727e178bf860caa14846d2a80f5f
Nathan P <natepie04@gmail.com>
2023-06-22 00:27:53 -0700

    Added description and intents to bot_config.py

--- config/bot_config.py
+++ config/bot_config.py
@@ -1,2 +1,9 @@
+import discord
+
 PREFIX = '!'
 
+INTENTS = discord.Intents.default()
+INTENTS.messages = True
+
+BOT_DESCRIPTION = 'A bot which can be configured to display information ' \
+                  'about users on a specific Minecraft Server'
