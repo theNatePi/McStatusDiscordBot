07eb32f856aa67c4c7b612b94ee91fee77d28678
Nathan P <natepie04@gmail.com>
2023-06-24 22:55:01 -0700

    Add docstrings and type annotations

--- run.py
+++ run.py
@@ -1,23 +1,42 @@
+"""
+The main module for mcstatus-discord bot
+Should be run using "python3.11+ run.py"
+
+mcstatus-discordbot is a Discord Bot which displays simple information
+about a configured minecraft server
+"""
 import asyncio
+
+import discord
+
 import config
 import cogs
 from discord.ext import commands
+from typing import List
 
 
 COGS = [cogs.Standard, cogs.McStatus, cogs.LoopedTasks]
 """All cogs to add to the bot"""
 
 
-def _start_background_tasks(cogs_to_start):
+def _start_background_tasks(cogs_to_start: List[commands.Cog]):
+    """
+    If a cog has background tasks, start them
+    Cogs with background tasks need a "start_tasks" method
+
+    :param cogs_to_start: Cogs to check for start_tasks method
+    """
     for cog in cogs_to_start:
         if hasattr(cog, 'start_tasks'):
             cog.start_tasks()
 
 
-def _define_on_ready(bot, added_cogs):
+def _define_on_ready(bot: commands.Bot, added_cogs: List[commands.Cog]):
     """
     Defines the "on_ready" event for the bot
+
     :param bot: commands.Bot object
+    :param added_cogs: A list of cogs added to the bot
     """
     @bot.event
     async def on_ready():
@@ -25,10 +44,12 @@ def _define_on_ready(bot, added_cogs):
         _start_background_tasks(added_cogs)
 
 
-async def _add_cogs(bot):
+async def _add_cogs(bot: commands.Bot) -> List[commands.Cog]:
     """
     Adds all cogs stored in COGS
+
     :param bot: commands.Bot object
+    :return initialized_cogs: All cogs initialized (added to the bot)
     """
     initialized_cogs = []
     for cog in COGS:
